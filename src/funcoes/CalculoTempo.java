2467e24a9ab3ac87fab8a003b3c2f9c270968995
Luiz Ângelo Martins <Luiz ngelo Martins@LuizAngelo>
2021-01-10 21:45:16 -0200

    Commit feito para registrar o início do projeto:
    	- Criação da classe para calculo do tempo.
    	- Criação do "form" de exibição geral do programa.

---
+++ src/funcoes/CalculoTempo.java
@@ -0,0 +1,21 @@
+
+
+
+    // [ "CRIADOR: LUIZ ÂNGELO MARTINS" ]:
+
+
+
+// PACOTE DA CLASSE:
+package funcoes;
+
+
+// MÉTODO PRINCIPAL DA CLASSE:
+public class CalculoTempo {
+    
+    // IMPLEMENTE SEU CÓDIGO AQUI!
+    
+}
+
+
+
+
